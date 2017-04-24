5f60620cf9af42b576cea031f8856a2b627a216d
amalakamala <amalakamala.a@gmail.com>
2017-04-24 20:12:47 -0300

    initia commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
 function convertirCelsius(conect){
  	var conect = document.getElementById('farenheit').value;
-	alert ("Tus Grados Celsius son : " + (Math.round((conect - 32) * 5/9)));
+ 	if(conect == 0 || conect == null){
+ 		alert ("Escribe un número");
+ 	}else{
+		alert ("Tus Grados Celsius son : " + (Math.round((conect - 32) * 5/9)));
+	}
 };
 
